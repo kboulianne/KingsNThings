5e9ab0aa2c9f8184a5652ef496266dd86eea1b75
Kurtis Boulianne <kurtis@kboulianne.com>
2014-01-17 12:04:49 -0500

    Game logic preparations.

---
+++ KingNThings/src/com/services/game/GameService.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.services.game;
+
+/**
+ *
+ * @author kurtis
+ */
+public class GameService {
+    // This class is responsible for managing game state and networking
+}
