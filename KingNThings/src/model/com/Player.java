c42474fad4bfe67ba032774440bb0866c176c778
Jason Fleischer <jason_fleischer@hotmail.ca>
2014-01-18 10:49:46 -0500

    player update

--- KingNThings/src/model/com/Player.java
+++ KingNThings/src/model/com/Player.java
@@ -3,13 +3,22 @@ package model.com;
 import javafx.scene.paint.Color;
 
 public class Player {
-	int id; 		// {1, 2, 3, 4}
+	PlayerId id; 		// {1, 2, 3, 4}
 	Color color;	// {blue, green, red, yellow}
 	Block block;
 	private String name;
 	
-	public Player(String name) {
+	public enum PlayerId { ONE(Color.BLUE),TWO(Color.GREEN),THREE(Color.RED),FOUR(Color.YELLOW); 
+		private final Color color;
+		PlayerId(Color c) {
+			color = c;
+		}
+	}
+	
+	public Player(PlayerId id, String name) {
 	    this.name = name;
+	    color = id.color;
+	    block = new Block();
 	}
 	
 	public final String getName() {
