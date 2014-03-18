443ef69a80ca500972b4dc05657d55cf0e25e401
kboulianne <kurtis@kboulianne.com>
2014-03-18 13:28:08 -0400

    missing import

--- KingNThings/src/com/view/PlayerInfoView.java
+++ KingNThings/src/com/view/PlayerInfoView.java
@@ -14,6 +14,7 @@ import com.view.customcontrols.Rack;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
